977368da8a988abab0acbbd1ac609e7ba0c0433c
Tyler Sutterley <tsutterl@uw.edu>
2026-08-17 12:26:39 -0700

    test: Peltier's homepage is down

--- test/test_download_and_read.py
+++ test/test_download_and_read.py
@@ -1,49 +1,69 @@
 #!/usr/bin/env python
-u"""
+"""
 test_download_and_read.py (11/2021)
 Tests the read program to verify that coefficients are being extracted
 """
+
 import pytest
 import pathlib
 import posixpath
 import gravity_toolkit as gravtk
 
+
 # PURPOSE: Download a GRACE file from PO.DAAC Cumulus and check that read program runs
-def test_podaac_cumulus_download_and_read(username,password):
+def test_podaac_cumulus_download_and_read(username, password):
     # find the path to the data files
     ids, urls, mtimes = gravtk.utilities.cmr(
-        mission='grace', center='CSR', release='RL06', level='L2',
-        product='GSM', start_date='2002-04-01', end_date='2002-04-30',
-        provider='POCLOUD', endpoint='data')
+        mission='grace',
+        center='CSR',
+        release='RL06',
+        level='L2',
+        product='GSM',
+        start_date='2002-04-01',
+        end_date='2002-04-30',
+        provider='POCLOUD',
+        endpoint='data',
+    )
     # attempt to download the GRACE file
     try:
         # build opener for data client access
         URS = 'urs.earthdata.nasa.gov'
-        opener = gravtk.utilities.attempt_login(URS,
-            username=username, password=password,
-            authorization_header=False, verbose=True)
+        opener = gravtk.utilities.attempt_login(
+            URS,
+            username=username,
+            password=password,
+            authorization_header=False,
+            verbose=True,
+        )
         # download and read as virtual file object
         FILE = gravtk.utilities.from_http(urls[0], context=None, verbose=True)
     except gravtk.utilities.urllib2.HTTPError as exc:
         pytest.xfail(exc.reason)
-    except EOFError as exc:
-        pytest.xfail("NASA Earthdata Login Error")
+    except (EOFError, gravtk.utilities.urllib2.URLError) as exc:
+        pytest.xfail('NASA Earthdata Login Error')
     # read as virtual file object
     Ylms = gravtk.read_GRACE_harmonics(FILE, 60)
     keys = ['time', 'start', 'end', 'clm', 'slm', 'eclm', 'eslm', 'header']
     test = dict(start=2452369.5, end=2452394.5)
     assert all((key in Ylms.keys()) for key in keys)
-    assert all((Ylms[key] == val) for key,val in test.items())
-    assert (Ylms['clm'][2,0] == -0.484169355584e-03)
+    assert all((Ylms[key] == val) for key, val in test.items())
+    assert Ylms['clm'][2, 0] == -0.484169355584e-03
+
 
 # PURPOSE: Download a GRACE file from GFZ and check that read program runs
 def test_gfz_http_download_and_read():
-    HOST=['https://isdc-data.gfz.de','grace','Level-2','CSR','RL06',
-        'GSM-2_2002095-2002120_GRAC_UTCSR_BA01_0600.gz']
+    HOST = [
+        'https://isdc-data.gfz.de',
+        'grace',
+        'Level-2',
+        'CSR',
+        'RL06',
+        'GSM-2_2002095-2002120_GRAC_UTCSR_BA01_0600.gz',
+    ]
     # attempt to download the GRACE file
     try:
         # download and read as virtual file object
-        FILE = gravtk.utilities.from_http(HOST,verbose=True)
+        FILE = gravtk.utilities.from_http(HOST, verbose=True)
     except gravtk.utilities.urllib2.HTTPError as exc:
         pytest.xfail(exc.reason)
     # read as virtual file object
@@ -51,14 +71,21 @@ def test_gfz_http_download_and_read():
     keys = ['time', 'start', 'end', 'clm', 'slm', 'eclm', 'eslm', 'header']
     test = dict(start=2452369.5, end=2452394.5)
     assert all((key in Ylms.keys()) for key in keys)
-    assert all((Ylms[key] == val) for key,val in test.items())
-    assert (Ylms['clm'][2,0] == -0.484169355584e-03)
+    assert all((Ylms[key] == val) for key, val in test.items())
+    assert Ylms['clm'][2, 0] == -0.484169355584e-03
+
 
 # PURPOSE: Download a GRACE file from GFZ and check that read program runs
-@pytest.mark.skip(reason="Deprecated GFZ FTP server")
+@pytest.mark.skip(reason='Deprecated GFZ FTP server')
 def test_gfz_ftp_download_and_read():
-    HOST=['isdcftp.gfz-potsdam.de','grace','Level-2','CSR','RL06',
-        'GSM-2_2002095-2002120_GRAC_UTCSR_BA01_0600.gz']
+    HOST = [
+        'isdcftp.gfz-potsdam.de',
+        'grace',
+        'Level-2',
+        'CSR',
+        'RL06',
+        'GSM-2_2002095-2002120_GRAC_UTCSR_BA01_0600.gz',
+    ]
     # attempt to download the GRACE file
     try:
         # download and read as virtual file object
@@ -70,17 +97,23 @@ def test_gfz_ftp_download_and_read():
     keys = ['time', 'start', 'end', 'clm', 'slm', 'eclm', 'eslm', 'header']
     test = dict(start=2452369.5, end=2452394.5)
     assert all((key in Ylms.keys()) for key in keys)
-    assert all((Ylms[key] == val) for key,val in test.items())
-    assert (Ylms['clm'][2,0] == -0.484169355584e-03)
+    assert all((Ylms[key] == val) for key, val in test.items())
+    assert Ylms['clm'][2, 0] == -0.484169355584e-03
+
 
 # PURPOSE: Download a GRACE-FO COST-G file from the GFZ ICGEM
 def test_gfz_icgem_costg_download_and_read():
-    HOST=['https://icgem.gfz.de','getseries','02_COST-G_',
-        'Grace-FO_RL02','GSM-2_2018152-2018181_GRFO_COSTG_BF01_0200.gfc']
+    HOST = [
+        'https://icgem.gfz.de',
+        'getseries',
+        '02_COST-G_',
+        'Grace-FO_RL02',
+        'GSM-2_2018152-2018181_GRFO_COSTG_BF01_0200.gfc',
+    ]
     # attempt to download the GRACE file
     try:
         # download and read as virtual file object
-        FILE = gravtk.utilities.from_http(HOST,verbose=True)
+        FILE = gravtk.utilities.from_http(HOST, verbose=True)
     except gravtk.utilities.urllib2.HTTPError as exc:
         pytest.xfail(exc.reason)
     # read as virtual file object
@@ -88,22 +121,23 @@ def test_gfz_icgem_costg_download_and_read():
     keys = ['time', 'start', 'end', 'clm', 'slm', 'eclm', 'eslm', 'header']
     test = dict(start=2458270.5, end=2458299.5)
     assert all((key in Ylms.keys()) for key in keys)
-    assert all((Ylms[key] == val) for key,val in test.items())
-    assert (Ylms['clm'][2,0] == -0.484165368910e-03)
+    assert all((Ylms[key] == val) for key, val in test.items())
+    assert Ylms['clm'][2, 0] == -0.484165368910e-03
+
 
 # PURPOSE: Download a Swarm file from ESA and check that read program runs
 def test_esa_swarm_download_and_read():
     # build url for Swarm file
-    HOST='https://swarm-diss.eo.esa.int'
-    swarm_file='SW_OPER_EGF_SHA_2__20131201T000000_20131231T235959_0101.ZIP'
-    parameters = gravtk.utilities.urlencode({'file':
-        posixpath.join('swarm','Level2longterm','EGF',swarm_file)})
-    remote_file = [HOST,'?do=download&{0}'.format(parameters)]    
+    HOST = 'https://swarm-diss.eo.esa.int'
+    swarm_file = 'SW_OPER_EGF_SHA_2__20131201T000000_20131231T235959_0101.ZIP'
+    parameters = gravtk.utilities.urlencode(
+        {'file': posixpath.join('swarm', 'Level2longterm', 'EGF', swarm_file)}
+    )
+    remote_file = [HOST, '?do=download&{0}'.format(parameters)]
     # attempt to download the Swarm file
     try:
         # download as local file object
-        gravtk.utilities.from_http(remote_file,
-            local=swarm_file, verbose=True)
+        gravtk.utilities.from_http(remote_file, local=swarm_file, verbose=True)
     except gravtk.utilities.urllib2.HTTPError as exc:
         pytest.xfail(exc.reason)
     # read the local file
@@ -112,16 +146,24 @@ def test_esa_swarm_download_and_read():
     keys = ['time', 'start', 'end', 'clm', 'slm', 'eclm', 'eslm']
     test = dict(start=2456627.5, end=2456658.499988426)
     assert all((key in Ylms.keys()) for key in keys)
-    assert all((Ylms[key] == val) for key,val in test.items())
-    assert (Ylms['clm'][2,0] == -0.48416530506600003e-03)
+    assert all((Ylms[key] == val) for key, val in test.items())
+    assert Ylms['clm'][2, 0] == -0.48416530506600003e-03
     # clean up
     swarm_file.unlink()
 
+
 # PURPOSE: Download a GRACE ITSG GRAZ file and check that read program runs
 def test_itsg_graz_download_and_read():
-    HOST=['http://ftp.tugraz.at','pub','ITSG','GRACE',
-        'ITSG-Grace_operational','monthly','monthly_n60',
-        'ITSG-Grace_operational_n60_2018-06.gfc']
+    HOST = [
+        'http://ftp.tugraz.at',
+        'pub',
+        'ITSG',
+        'GRACE',
+        'ITSG-Grace_operational',
+        'monthly',
+        'monthly_n60',
+        'ITSG-Grace_operational_n60_2018-06.gfc',
+    ]
     # attempt to download the GRAZ file
     try:
         # download as local file object
@@ -134,7 +176,7 @@ def test_itsg_graz_download_and_read():
     keys = ['time', 'start', 'end', 'clm', 'slm', 'eclm', 'eslm']
     test = dict(start=2458270.5, end=2458300.499988426)
     assert all((key in Ylms.keys()) for key in keys)
-    assert all((Ylms[key] == val) for key,val in test.items())
-    assert (Ylms['clm'][2,0] == -0.4841694727612e-03)
+    assert all((Ylms[key] == val) for key, val in test.items())
+    assert Ylms['clm'][2, 0] == -0.4841694727612e-03
     # clean up
     itsg_file.unlink()
--- test/test_time.py
+++ test/test_time.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-u"""
+"""
 test_time.py (01/2023)
 Verify time conversion and utility functions
 
@@ -11,126 +11,140 @@ UPDATE HISTORY:
         test date parser for cases when only a date and no units
     Written 12/2020
 """
+
 import pytest
 import numpy as np
 import gravity_toolkit as gravtk
 
-# parameterize calendar dates
-@pytest.mark.parametrize("YEAR", np.random.randint(1992,2020,size=2))
-@pytest.mark.parametrize("MONTH", np.random.randint(1,13,size=2))
-# PURPOSE: verify forward and backwards time conversions
-def test_julian(YEAR,MONTH):
-    # days per month in a leap and a standard year
-    # only difference is February (29 vs. 28)
-    dpm_leap = np.array([31,29,31,30,31,30,31,31,30,31,30,31])
-    dpm_stnd = np.array([31,28,31,30,31,30,31,31,30,31,30,31])
-    DPM = dpm_stnd if np.mod(YEAR,4) else dpm_leap
-    assert (np.sum(DPM) == gravtk.time.calendar_days(YEAR).sum())
-    # calculate Modified Julian Day (MJD) from calendar date
-    DAY = np.random.randint(1,DPM[MONTH-1]+1)
-    HOUR = np.random.randint(0,23+1)
-    MINUTE = np.random.randint(0,59+1)
-    SECOND = 60.0*np.random.random_sample(1)
-    MJD = gravtk.time.convert_calendar_dates(YEAR, MONTH, DAY,
-        hour=HOUR, minute=MINUTE, second=SECOND,
-        epoch=(1858,11,17,0,0,0))
-    # convert MJD to calendar date
-    JD = np.squeeze(MJD) + 2400000.5
-    YY,MM,DD,HH,MN,SS = gravtk.time.convert_julian(JD,
-        format='tuple', astype=np.float64)
-    # assert dates
-    eps = np.finfo(np.float16).eps
-    assert (YY == YEAR)
-    assert (MM == MONTH)
-    assert (DD == DAY)
-    assert (HH == HOUR)
-    assert (MN == MINUTE)
-    assert (np.abs(SS - SECOND) < eps)
 
 # parameterize calendar dates
-@pytest.mark.parametrize("YEAR", np.random.randint(1992,2020,size=2))
-@pytest.mark.parametrize("MONTH", np.random.randint(1,13,size=2))
+@pytest.mark.parametrize('YEAR', np.random.randint(1992, 2020, size=2))
+@pytest.mark.parametrize('MONTH', np.random.randint(1, 13, size=2))
 # PURPOSE: verify forward and backwards time conversions
-def test_decimal_dates(YEAR,MONTH):
+def test_julian(YEAR, MONTH):
     # days per month in a leap and a standard year
     # only difference is February (29 vs. 28)
-    dpm_leap = np.array([31,29,31,30,31,30,31,31,30,31,30,31])
-    dpm_stnd = np.array([31,28,31,30,31,30,31,31,30,31,30,31])
-    DPM = dpm_stnd if np.mod(YEAR,4) else dpm_leap
-    assert (np.sum(DPM) == gravtk.time.calendar_days(YEAR).sum())
+    dpm_leap = np.array([31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
+    dpm_stnd = np.array([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
+    DPM = dpm_stnd if np.mod(YEAR, 4) else dpm_leap
+    assert np.sum(DPM) == gravtk.time.calendar_days(YEAR).sum()
     # calculate Modified Julian Day (MJD) from calendar date
-    DAY = np.random.randint(1,DPM[MONTH-1]+1)
-    HOUR = np.random.randint(0,23+1)
-    MINUTE = np.random.randint(0,59+1)
-    SECOND = 60.0*np.random.random_sample(1)
+    DAY = np.random.randint(1, DPM[MONTH - 1] + 1)
+    HOUR = np.random.randint(0, 23 + 1)
+    MINUTE = np.random.randint(0, 59 + 1)
+    SECOND = 60.0 * np.random.random_sample(1)
+    MJD = gravtk.time.convert_calendar_dates(
+        YEAR,
+        MONTH,
+        DAY,
+        hour=HOUR,
+        minute=MINUTE,
+        second=SECOND,
+        epoch=(1858, 11, 17, 0, 0, 0),
+    )
+    # convert MJD to calendar date
+    JD = np.squeeze(MJD) + 2400000.5
+    YY, MM, DD, HH, MN, SS = gravtk.time.convert_julian(
+        JD, format='tuple', astype=np.float64
+    )
+    # assert dates
+    eps = np.finfo(np.float16).eps
+    assert YY == YEAR
+    assert MM == MONTH
+    assert DD == DAY
+    assert HH == HOUR
+    assert MN == MINUTE
+    assert np.abs(SS - SECOND) < eps
+
+
+# parameterize calendar dates
+@pytest.mark.parametrize('YEAR', np.random.randint(1992, 2020, size=2))
+@pytest.mark.parametrize('MONTH', np.random.randint(1, 13, size=2))
+# PURPOSE: verify forward and backwards time conversions
+def test_decimal_dates(YEAR, MONTH):
+    # days per month in a leap and a standard year
+    # only difference is February (29 vs. 28)
+    dpm_leap = np.array([31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
+    dpm_stnd = np.array([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
+    DPM = dpm_stnd if np.mod(YEAR, 4) else dpm_leap
+    assert np.sum(DPM) == gravtk.time.calendar_days(YEAR).sum()
+    # calculate Modified Julian Day (MJD) from calendar date
+    DAY = np.random.randint(1, DPM[MONTH - 1] + 1)
+    HOUR = np.random.randint(0, 23 + 1)
+    MINUTE = np.random.randint(0, 59 + 1)
+    SECOND = 60.0 * np.random.random_sample(1)
     # calculate year-decimal time
-    tdec = gravtk.time.convert_calendar_decimal(YEAR, MONTH, day=DAY,
-        hour=HOUR, minute=MINUTE, second=SECOND)
+    tdec = gravtk.time.convert_calendar_decimal(
+        YEAR, MONTH, day=DAY, hour=HOUR, minute=MINUTE, second=SECOND
+    )
     # day of the year 1 = Jan 1, 365 = Dec 31 (std)
-    day_temp = np.mod(tdec, 1)*np.sum(DPM)
+    day_temp = np.mod(tdec, 1) * np.sum(DPM)
     DofY = np.floor(day_temp) + 1
     # cumulative sum of the calendar dates
-    day_cumulative = np.cumsum(np.concatenate(([0],DPM))) + 1
+    day_cumulative = np.cumsum(np.concatenate(([0], DPM))) + 1
     # finding which month date is in
     i = np.nonzero((DofY >= day_cumulative[0:-1]) & (DofY < day_cumulative[1:]))
-    month_range = np.arange(1,13)
+    month_range = np.arange(1, 13)
     month = month_range[i]
     # finding day of the month
     day = (DofY - day_cumulative[i]) + 1
     # convert residuals into time (hour, minute and second)
-    hour_temp = np.mod(day_temp,1)*24.0
-    minute_temp = np.mod(hour_temp,1)*60.0
-    second = np.mod(minute_temp,1)*60.0
+    hour_temp = np.mod(day_temp, 1) * 24.0
+    minute_temp = np.mod(hour_temp, 1) * 60.0
+    second = np.mod(minute_temp, 1) * 60.0
     # assert dates
     eps = np.finfo(np.float16).eps
-    assert (np.floor(tdec) == YEAR)
-    assert (month == MONTH)
-    assert (day == DAY)
-    assert (np.floor(hour_temp) == HOUR)
-    assert (np.floor(minute_temp) == MINUTE)
-    assert (np.abs(second - SECOND) < eps)
+    assert np.floor(tdec) == YEAR
+    assert month == MONTH
+    assert day == DAY
+    assert np.floor(hour_temp) == HOUR
+    assert np.floor(minute_temp) == MINUTE
+    assert np.abs(second - SECOND) < eps
+
 
 # PURPOSE: test UNIX time
 def test_unix_time():
     # ATLAS Standard Data Epoch
     UNIX = gravtk.utilities.get_unix_time('2018-01-01 00:00:00')
-    assert (UNIX == 1514764800)
+    assert UNIX == 1514764800
     # check UNIX time conversion with delta times
-    output_time = gravtk.time.convert_delta_time(UNIX,
-        epoch1=gravtk.time._unix_epoch, epoch2=(2018,1,1),
-        scale=1.0)
-    assert (output_time == 0)
+    output_time = gravtk.time.convert_delta_time(
+        UNIX, epoch1=gravtk.time._unix_epoch, epoch2=(2018, 1, 1), scale=1.0
+    )
+    assert output_time == 0
+
 
 # PURPOSE: test parsing time strings
 def test_parse_date_string():
     # time string for Modified Julian Days
     time_string = 'days since 1858-11-17T00:00:00'
-    epoch,to_secs = gravtk.time.parse_date_string(time_string)
+    epoch, to_secs = gravtk.time.parse_date_string(time_string)
     # check the epoch and the time unit conversion factors
-    assert np.all(epoch == [1858,11,17,0,0,0])
-    assert (to_secs == 86400.0)
+    assert np.all(epoch == [1858, 11, 17, 0, 0, 0])
+    assert to_secs == 86400.0
     # time string for ATLAS Standard Data Epoch
     time_string = 'seconds since 2018-01-01T00:00:00'
-    epoch,to_secs = gravtk.time.parse_date_string(time_string)
+    epoch, to_secs = gravtk.time.parse_date_string(time_string)
     # check the epoch and the time unit conversion factors
-    assert np.all(epoch == [2018,1,1,0,0,0])
-    assert (to_secs == 1.0)
+    assert np.all(epoch == [2018, 1, 1, 0, 0, 0])
+    assert to_secs == 1.0
     # time string for unitless case
     time_string = '2000-01-01T12:00:00'
-    epoch,to_secs = gravtk.time.parse_date_string(time_string)
+    epoch, to_secs = gravtk.time.parse_date_string(time_string)
     # check the epoch and the time unit conversion factors
-    assert np.all(epoch == [2000,1,1,12,0,0])
-    assert (to_secs == 0.0)
+    assert np.all(epoch == [2000, 1, 1, 12, 0, 0])
+    assert to_secs == 0.0
     # time string for unitless case with a time zone
     time_string = '2000-01-01T12:00:00.000-06:00'
-    epoch,to_secs = gravtk.time.parse_date_string(time_string)
+    epoch, to_secs = gravtk.time.parse_date_string(time_string)
     # check the epoch and the time unit conversion factors
-    assert np.all(epoch == [2000,1,1,18,0,0])
-    assert (to_secs == 0.0)
+    assert np.all(epoch == [2000, 1, 1, 18, 0, 0])
+    assert to_secs == 0.0
+
 
 # PURPOSE: test months adjustment for special cases
 # parameterize calendar dates
-@pytest.mark.parametrize("PROC", ['CSR','GFZ','GSFC','JPL'])
+@pytest.mark.parametrize('PROC', ['CSR', 'GFZ', 'GSFC', 'JPL'])
 def test_adjust_months(PROC):
     # The 'Special Months' (Nov 2011, Dec 2011 and April 2012) with
     # Accelerometer shutoffs make the relation between month number
@@ -141,7 +155,7 @@ def test_adjust_months(PROC):
     # For GSFC: Oct 2018 (202) is centered in Nov 2018 (203)
 
     # dates with special months for each processing center
-    center_dates = dict(CSR=[],GFZ=[],GSFC=[],JPL=[])
+    center_dates = dict(CSR=[], GFZ=[], GSFC=[], JPL=[])
     # CSR dates to test (year-decimal, GRACE month)
     center_dates['CSR'].append([2011.62465753, 116])
     center_dates['CSR'].append([2011.70821918, 117])
@@ -196,9 +210,9 @@ def test_adjust_months(PROC):
     center_dates['JPL'].append([2015.62465753, 164])
 
     # get dates and months for center
-    tdec,months = np.transpose(center_dates[PROC])
+    tdec, months = np.transpose(center_dates[PROC])
     # GRACE/GRACE-FO months with duplicates
-    temp = np.array(12.0*(tdec-2002.0)+1,dtype='i')
+    temp = np.array(12.0 * (tdec - 2002.0) + 1, dtype='i')
     assert np.any(temp != months.astype('i'))
     # run months adjustment to fix special cases
     temp = gravtk.time.adjust_months(temp)
